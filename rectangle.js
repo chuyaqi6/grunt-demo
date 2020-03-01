01b7015400d1486969a3ae53309202afd67e79bd
chuyaqi6 <2484917266@qq.com>
2020-03-01 18:43:46 +0800

    mocha-fe

--- rectangle.js
+++ rectangle.js
@@ -13,8 +13,12 @@ $(function(){
         h = Number($height.val());
     
     //calculate
+    /*
     var p = 2 * (w + h),
         a = w * h;
+    */
+    var rect = rectangle();
+
     /**
      * roundFractional
      * 保留小数点后n位
@@ -22,13 +26,14 @@ $(function(){
      * @param n 小数点后第n位
      * @returns 近似处理后的值
      */
-    
+    /*
     function roundFractional(x,n){
       return Math.round(x*Math.pow(10,n))/Math.pow(10,n);
     }
     p = roundFractional(p,2);
     a = roundFractional(a,2);
-    $perimeter.val(p);
-    $area.val(a);
+    */
+    $perimeter.val(rect.perimeter(w,h));
+    $area.val(rect.area(w,h));
   });
 });
